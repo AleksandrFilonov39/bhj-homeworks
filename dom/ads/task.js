d6fc31577f6b51000d08973a8ea403c5a4a8567b
Filonov <ABFilonov@yandex.ru>
2025-04-21 10:12:30 +0200

    Home work 3.2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,81 +1,92 @@
 window.addEventListener('DOMContentLoaded', () => {
-    class Rotate {
-        constructor (selector, selectorActiveClass) {
-            this.selectors = document.querySelectorAll(selector),
-            this.selectorActiveClass = selectorActiveClass,
-            this.i = 0
-        }
 
-        rotate () {
+//----- Use Class----------
+    // class Rotate {
+    //     constructor (selector, selectorActiveClass) {
+    //         this.selectors = document.querySelectorAll(selector),
+    //         this.selectorActiveClass = selectorActiveClass,
+    //         this.i = 0
+
+    //         this.rotate = this.rotate.bind(this);
+    //     }
+
+    //     rotate () {
             
-            if(this.i === this.selectors.length){
-                this.selectors[this.i - 1].classList.remove( this.selectorActiveClass)
-                this.i = 0;
+    //         if(this.i === this.selectors.length){
+    //             this.selectors[this.i - 1].classList.remove( this.selectorActiveClass)
+    //             this.i = 0;
+    //         };
+    //         this.selectors[this.i].style.color = this.selectors[this.i].dataset.color;
+    //         if (this.i === 0 ) {
+    //             this.selectors[this.i].classList.add( this.selectorActiveClass)
+    //             this.i++
+    //         } else if (this.i > 0 && this.i < this.selectors.length) {
+    //             this.selectors[this.i - 1].classList.remove( this.selectorActiveClass)
+    //             this.selectors[this.i].classList.add( this.selectorActiveClass)
+    //             this.i++;
+    //         } 
+    //     }
+
+    //     setIntervalTime () {
+    //         if(this.selectors[this.i]) {           
+    //         return Number(this.selectors[this.i].dataset.speed);
+    //         }
+    //     }
+
+    //     start() {
+    //         const rotateNext = () => {
+    //             this.rotate();
+    //             const delay = this.setIntervalTime();
+    //             setTimeout(rotateNext, delay);
+    //         };
+    //         rotateNext();
+    //     }
+    // }
+
+    // const rotate = new Rotate ('.rotator__case', 'rotator__case_active');
+
+    // rotate.start();
+
+
+
+//------------------Use function ----------------------//
+
+    function start (selector, activeClass) {
+        const words = document.querySelectorAll(selector);
+
+            let i = 0;
+
+        function rotate () {
+            if(i === words.length){
+                words[i - 1].classList.remove(activeClass)
+                i = 0;
             };
-            this.selectors[this.i].style.color = this.selectors[this.i].dataset.color;
-            if (this.i === 0 ) {
-                this.selectors[this.i].classList.add( this.selectorActiveClass)
-                this.i++
-            } else if (this.i > 0 && this.i < this.selectors.length) {
-                this.selectors[this.i - 1].classList.remove( this.selectorActiveClass)
-                this.selectors[this.i].classList.add( this.selectorActiveClass)
-                this.i++;
-            } 
+
+            words[i].style.color = words[i].dataset.color;
+
+            if (i === 0 ) {
+                words[i].classList.add(activeClass)
+                i++
+            } else if (i > 0 && i < words.length) {
+                words[i - 1].classList.remove(activeClass)
+                words[i].classList.add(activeClass)
+                i++;
+            }   
+
+            let delay = setDelay ();
+           return setTimeout(rotate, delay);
         }
 
-        setIntervalTime () {
-            if(this.selectors[this.i]) {           
-            return Number(this.selectors[this.i].dataset.speed);
-            }
+        function setDelay () {
+            if(words[i]) {           
+                return Number(words[i].dataset.speed);
+            };
         }
 
-        start () {
-            let timeInterval = this.setIntervalTime ();
-                setInterval(() => {
-                    timeInterval = this.setIntervalTime ();
-                this.rotate ();
-            }, timeInterval);
-        }
+       return rotate ();
     }
 
-    const rotate = new Rotate ('.rotator__case', 'rotator__case_active');
-
-    rotate.start();
-
-
-
-//------------------First Work ----------------------//
-// const words = document.querySelectorAll('.rotator__case');
-
-// function start () {
-//         let i = 0;
-
-//     function rotate () {
-//         words[i].style.color = words[i].dataset.color;
-//         if (i === 0 ) {
-//             words[i].classList.add('rotator__case_active')
-//             i++
-//         } else if (i > 0 && i < words.length) {
-//             words[i - 1].classList.remove('rotator__case_active')
-//             words[i].classList.add('rotator__case_active')
-//             i++;
-//         } else {
-//             words[i - 1].classList.remove('rotator__case_active')
-//         i = 0;
-//             words[i].classList.add('rotator__case_active')
-//         }
-//             console.log(words[i])
-//             console.log(i)
-//     }
-    
-//     return rotate;
-// }
-
-// const newStart = start ();
-
-//     setInterval(() => {
-//         newStart();
-//     }, 1000);
+    start ('.rotator__case', 'rotator__case_active');
 
 });
 
--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const fraseInText = document.querySelectorAll('.reveal');
-
+    
     document.addEventListener('scroll', () => {
         fraseInText.forEach((el) => {
            const {top, bottom} = el.getBoundingClientRect();
-            if(top > 0 || bottom > 0) {
-                el.classList.add('reveal_active');
-            }
+        (bottom > 0 && top <  window.innerHeight) ? el.classList.add('reveal_active') : el.classList.remove('reveal_active');
         });
     })
 });
